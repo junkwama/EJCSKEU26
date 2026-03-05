a0935eb3670d298f9d0679169e68202f597f9edb
Judy Nkwama <74412890+Judy-Nkwama@users.noreply.github.com>
2026-03-05 16:47:36 +0100

    Code gardening

--- utils/constants.py
+++ utils/constants.py
@@ -2,8 +2,9 @@ from enum import Enum
 
 class Regex(Enum):
     EMAIL = r"^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$"
-    PHONE = r"^\+?[1-9]\d{1,14}$"
+    PHONE = r"^\+[1-9]\d{1,14}$"
 
 class ProjDepth(Enum):
     SHALLOW = "shallow"
     FLAT = "flat"
+    
